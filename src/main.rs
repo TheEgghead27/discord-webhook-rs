1a31fac559d9f152f67338622c4e161e4f49f10e
David Chen <dchen07@protonmail.com>
2022-09-06 21:24:11 -0400

    Add initial working webhook script
    Takes in message text (erroneously required at this moment) by
    opening an EDITOR buffer to a temporary file.
    
    Filenames are provided to it as arguments (argv), and the bytes are
    loaded synchronously (due to filename lifetimes) using `tokio::fs` to be
    *POSTed* asynchronously to the array of (Discord) webhook URLs.

---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use std::env;
+use std::path::PathBuf;
+// use std::fs;
+use tokio::fs;
+use futures::future::join_all;
+// use reqwest::header::HeaderMap;
+// use reqwest::header::HeaderValue;
+use reqwest::multipart::Part;
+
+static BUF_NAME: &str = "/tmp/webhook.tmp";
+
+struct File {
+    name: PathBuf,
+    data: Vec<u8>
+}
+
+#[tokio::main]
+async fn main() {
+    let mut files: Vec<File> = Vec::new();
+    for (i, arg) in env::args().enumerate() {
+        if (i < 1) || 
+            // don't read "dev" if we're in `cargo run dev`
+            (cfg!(debug_assertions) && i == 1 && arg == "dev") {
+            continue;
+        }
+        // println!("{i} {arg}");
+        files.push(
+            File {
+                name: PathBuf::from(&arg),
+                data: fs::read(arg).await.unwrap(),
+            }
+        );
+    }
+
+    
+	let u = [
+        "https://discord.com/api/webhooks/REDACTED",
+    ];
+    let editor: String = match env::var("EDITOR") {
+        Ok(ed) => ed,
+        Err(_) => String::from("vim"),
+    };
+    std::process::Command::new(editor)
+        .arg(BUF_NAME)
+        // even though the docs say spawn() and status() should do the same thing with stdio,
+        // it seems to mess up stdin when using spawn() instead of status()
+        .status()
+        .expect("EDITOR failed to start - have you exported the environment variable?");
+
+
+    let contents = fs::read_to_string(BUF_NAME).await.unwrap()
+                // .trim() returns a &str which we dont own, and disappears with this String after
+                // this expression
+                // so instead we use .to_owned() to take ownership of it as a String
+                .trim().to_owned();
+    
+
+    let mut v = Vec::new();
+    for url in u {
+        v.push(req(&contents, &files, url, None));
+    }
+    
+    // https://stackoverflow.com/questions/63326882/how-to-wait-for-a-list-of-async-function-calls-in-rust
+    join_all(v).await;
+   
+}
+
+async fn req(msg: &String, files: &Vec<File>, url: &str, client: Option<reqwest::Client>) {
+    let client = client.unwrap_or(reqwest::Client::new());
+
+    /*
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", HeaderValue::from_static("application/json"));
+    */
+    let mut form = reqwest::multipart::Form::new()
+        .part("payload_json",
+              Part::text(format!("{{\"content\": \"{msg}\"}}"))
+              .mime_str("application/json")
+              .expect("Failed to set mime")
+        );
+    for (i, file) in files.iter().enumerate() {
+        form = form.part(format!("files[{i}]"),
+                         Part::bytes(file.data.to_owned())
+                         .file_name(file.name.file_name().unwrap().to_str().unwrap().to_owned()));
+        println!("Attached {} as files[{}]", file.name.display(), i);
+    }
+
+    let res = client.post(url)
+        // .body("{\"content\": \"testing testing 123\"}")
+        // .headers(headers)
+        .multipart(form)
+        .send().await;
+    match res {
+        Ok(resp) => {
+            println!("{:#?}", resp);
+            println!("{:#?}", resp.text().await);
+        },
+        Err(error) => panic!("uwupsies error: {:?}", error)
+    }
+}
