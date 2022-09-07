7d6890630ed3f93e35e1d48898031589326462a6
David Chen <dchen07@protonmail.com>
2022-09-07 13:40:08 -0400

    Add dynamic configuration, cleanup code
    Instead of manually editing the target URL array in the source code, the
    program now scans a TSV file for target URLs.
    
    Reformats the code using `cargo fmt` to meet standard conventions.

--- README.md
+++ README.md
@@ -2,18 +2,21 @@
 This is a little "script" I wrote for practice with Rust and asynchronous operations within Rust.
 
 ## Getting started
-Clone this GitHub repository, then `cd` into the project.
+Clone this GitHub repository, then `cd` into the project and compile it (unlike other languages, cargo automatically installs dependencies on build/run).
 ```sh
 git clone https://github.com/TheEgghead27/discord-webhook-rs.git
 cd discord-webhook-rs
-```
-
-From here, you need to edit the `u` array in `src/main.rs` to contain the Discord webhook URLs that you wish to target.
-
-After you've configured your target URLs, you can compile the program for general usage.
-```
 cargo build --release
 ```
+
+## Configuration
+The program expects a `webhooks.tsv` file at your `XDG_CONFIG_HOME` (normally `~/.config`), with a format as follows:
+```tsv
+WEBHOOK_URL	PREFIX(optional)
+```
+Note the use of `\t` tab-separated values instead of whitespaces.
+
+If you are only specifying `WEBHOOK_URL`, you do not have to include a tab.
 
 ## Invokation
 Simply type the built executable's filename, optionally followed by a list of files you wish to attach to your webhook message.
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,44 @@
+use futures::future::join_all;
+use reqwest::{
+    multipart::{Form, Part},
+    Client,
+};
 use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 use tokio::fs;
-use futures::future::join_all;
-use reqwest::{Client, multipart::{Form, Part}};
+use csv::ReaderBuilder;
 
 static BUF_NAME: &str = "/tmp/webhook.tmp";
 
+struct Destination {
+    url: String,
+    // simply use empty string if no prefix
+    prefix: String,
+}
+
 struct File {
     name: PathBuf,
-    data: Vec<u8>
+    data: Vec<u8>,
 }
 
 #[tokio::main]
 async fn main() {
     let mut files: Vec<File> = Vec::new();
     for (i, arg) in env::args().enumerate() {
-        if (i < 1) || 
+        if (i < 1) ||
             // don't read "dev" if we're in `cargo run dev`
-            (cfg!(debug_assertions) && i == 1 && arg == "dev") {
+            (cfg!(debug_assertions) && i == 1 && arg == "dev")
+        {
             continue;
         }
-        files.push(
-            File {
-                name: PathBuf::from(&arg),
-                data: fs::read(arg).await.unwrap(),
-            }
-        );
+        files.push(File {
+            name: PathBuf::from(&arg),
+            data: fs::read(arg).await.unwrap(),
+        });
     }
 
-    
-	let u = [
-        "https://discord.com/api/webhooks/REDACTED",
-    ];
+    let u = get_dest();
 
     let editor: String = match env::var("EDITOR") {
         Ok(ed) => ed,
@@ -45,48 +51,74 @@ async fn main() {
         .status()
         .expect("EDITOR failed to start - have you exported the environment variable?");
 
-
-    let contents = fs::read_to_string(BUF_NAME).await.unwrap()
-                // .trim() returns a &str which we dont own, and disappears with this String after
-                // this expression
-                // so instead we use .to_owned() to take ownership of it as a String
-                .trim().to_owned();
+    let contents = fs::read_to_string(BUF_NAME)
+        .await
+        .unwrap()
+        // .trim() returns a &str which we dont own, and disappears with this String after
+        // this expression
+        // so instead we use .to_owned() to take ownership of it as a String
+        .trim()
+        .to_owned();
 
     let client = Client::new();
 
     let mut v = Vec::new();
-    for url in u {
-        v.push(req(&contents, &files, url, &client));
+    for dest in u {
+        v.push(req(&contents, &files, dest, &client));
     }
-    
+
     // https://stackoverflow.com/questions/63326882/how-to-wait-for-a-list-of-async-function-calls-in-rust
     join_all(v).await;
-   
 }
 
-async fn req(msg: &String, files: &Vec<File>, url: &str, client: &reqwest::Client) {
-    let mut form = Form::new()
-        .part("payload_json",
-              Part::text(format!("{{\"content\": \"{msg}\"}}"))
-              .mime_str("application/json")
-              .expect("Failed to set mime")
-        );
-    
+fn get_dest() -> Vec<Destination> {
+    let path: String = env::var("XDG_CONFIG_HOME")
+        .unwrap_or(env::var("HOME").unwrap_or("~".to_string()) + "/.config/")
+        + "webhooks.tsv";
+    println!("path: {}", path);
+
+    let mut reader = ReaderBuilder::new()
+        .has_headers(false)
+        .delimiter(b'\t')
+        .flexible(true)
+        .from_path(path)
+        .expect("~/.config/webhooks.tsv exists");
+
+    let mut out: Vec<Destination> = Vec::new();
+    for result in reader.records() {
+        let record = result.unwrap();
+        out.push(Destination {
+            url: record[0].to_owned(),
+            prefix: String::from(record.get(1).unwrap_or("")),
+        });
+    }
+
+    return out;
+}
+
+async fn req(msg: &String, files: &Vec<File>, dest: Destination, client: &reqwest::Client) {
+    let mut form = Form::new().part(
+        "payload_json",
+        Part::text(format!("{{\"content\": \"{} {}\"}}", dest.prefix, msg))
+            .mime_str("application/json")
+            .expect("Failed to set mime"),
+    );
+
     for (i, file) in files.iter().enumerate() {
-        form = form.part(format!("files[{i}]"),
-                         Part::bytes(file.data.to_owned())
-                         .file_name(file.name.file_name().unwrap().to_str().unwrap().to_owned()));
+        form = form.part(
+            format!("files[{i}]"),
+            Part::bytes(file.data.to_owned())
+                .file_name(file.name.file_name().unwrap().to_str().unwrap().to_owned()),
+        );
         println!("Attached {} as files[{}]", file.name.display(), i);
     }
 
-    let res = client.post(url)
-        .multipart(form)
-        .send().await;
+    let res = client.post(dest.url).multipart(form).send().await;
     match res {
         Ok(resp) => {
             println!("{:#?}", resp);
             println!("{:#?}", resp.text().await);
-        },
-        Err(error) => panic!("uwupsies error: {:?}", error)
+        }
+        Err(error) => panic!("uwupsies error: {:?}", error),
     }
 }
